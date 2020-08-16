ac8f960565ff092c7ca1d064879568388e7691d4
Hailen <759430324@qq.com>
2020-08-16 22:39:01 +0800

    自定义树组件名称修改

--- admin/src/views/tree/component/MyTree.vue
+++ admin/src/views/tree/component/MyTree.vue
@@ -1,15 +1,12 @@
-import { Dropdown } from 'antd';
-import { watch } from '../../../../../../Example/资料/五子棋/hhh/pro/scripts/node';
 <template>
   <div class="tree">
     <el-tree
-      :data.sync="data"
+      :data="data"
       :props="defaultProps"
       show-checkbox
       :default-expand-all="true"
       :expand-on-click-node="false"
       :render-content="render"
-      @node-click="handleNodeClick"
       @check-change="handleCheckChange"
     />
   </div>
@@ -17,159 +14,212 @@ import { watch } from '../../../../../../Example/资料/五子棋/hhh/pro/script
 
 <script>
 export default {
-  name: 'CustomTree',
+  name: "CustomTree",
   props: {
     treeData: { type: Array, default: () => [] },
     defaultProps: { type: Object, default: () => {} },
     fileDrop: { type: Array, default: () => [] },
     directoryDrop: { type: Array, default: () => [] },
-    delete: Function
+    deleteTree: Function,
+    updateTree: Function,
   },
   data() {
     return {
-      data: this.treeData // 使用data接收父组件数据
-    }
+      data: this.treeData, // 使用data接收父组件数据
+      currentId: "", //当前编辑树的id
+      currentCont: "", //当前编辑树的内容
+    };
   },
   watch: {
     // 需要监听父组件传过来的treeData属性, 确保更新渲染el-tree组件
     treeData(data) {
-      this.data = data
-    }
+      this.data = data;
+    },
   },
   methods: {
     isParent(data) {
-      return data.type == 'parent'
+      return data.type == "parent";
     },
+    // 下拉菜单删除/修改判断
+    handleCommand(data, item) {
+      if (item.text === "rn") {
+        this.handleRename(data);
+      } else if (item.text === "rm") {
+        this.handleRemove(data);
+      }
+    },
+    // 重命名
     handleRename(data) {
-      // 重命名
-      console.log('data', data)
+      this.currentCont = data.name;
+      this.currentId = data.id;
+    },
+
+    // 删除文件
+    handleRemove(data) {
+      this.$confirm(`请确认是否删除:${data.name}?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          // 用户传递了删除方法直接调用, 没有就直接删除
+          this.deleteTree;
+          this.deleteTree
+            ? this.deleteTree(data.id)
+                .then((res) => {
+                  console.log("res", res);
+                  this.remove(data.id);
+                })
+                .catch((err) => {
+                  console.log("删除失败", err);
+                })
+            : this.remove(data.id);
+        })
+        .catch(() => {
+          this.$message.info("已取消删除");
+        });
     },
     // 执行删除方法
     del(arr, id) {
-      console.log('删除ID:', id)
+      console.log("删除ID:", id);
       arr.forEach((item, index) => {
         if (item.id === id) {
-          arr.splice(index, 1)
+          arr.splice(index, 1);
+          this.$message.success("修改成功!");
         } else if (item.id !== id && item.children) {
-          this.del(item.children, id)
+          this.del(item.children, id);
         }
-      })
+      });
     },
     // 删除页面的内容
     remove(id) {
       // 深拷贝数据
-      const list = JSON.parse(JSON.stringify(this.data))
-      console.log('所有节点树:', list)
-      this.del(list, id) // 删除
-      //   this.$emit("update:data", list);
-      this.data = list // 更新数据
+      const list = JSON.parse(JSON.stringify(this.data));
+      this.del(list, id); // 删除
+      // 更新数据
+      this.$emit("update:tree-data", list);
+      //   this.data = list;
     },
 
-    handleRemove(data) {
-      // 删除文件
-      console.log('删除data', data)
-      this.$confirm(`请确认是否删除:${data.name}?`, '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      })
-        .then(() => {
-          // 用户传递了删除方法直接调用, 没有就直接删除
-          this.delete
-            ? this.delete(data.id)
-              .then((res) => {
-                console.log('res', res)
-                this.remove(data.id)
-                this.$message.success('删除成功!')
-              })
-              .catch((err) => {
-                console.log('删除失败', err)
-              })
-            : this.remove(data.id)
-        })
-        .catch(() => {
-          this.$message.info('已取消删除')
-        })
+    // 修改输入框可输入处理
+    handleInput(val) {
+      this.currentCont = val;
     },
-    handleCommand(data, item) {
-      if (item.text === 'rn') {
-        this.handleRename(data)
-      } else if (item.text === 'rm') {
-        this.handleRemove(data)
-      }
+    // 执行修改方法
+    mod(arr, id) {
+      arr.forEach((item, index) => {
+        if (item.id === id) {
+          item.name = this.currentCont;
+          this.currentId = ""; //隐藏输入框
+          this.$message.success("修改成功!");
+        } else if (item.id !== id && item.children) {
+          this.mod(item.children, id);
+        }
+      });
     },
-    // render
+    // 修改树名称
+    ok(data) {
+      const list = JSON.parse(JSON.stringify(this.data));
+      // 用户传递了修改方法直接调用, 没有就直接修改
+      this.updateTree
+        ? this.updateTree(this.currentCont, this.currentCont)
+            .then((res) => {
+              console.log("修改成功", res);
+              this.mod(list, data.id);
+            })
+            .catch((err) => {
+              console.log("修改失败", err);
+            })
+        : this.mod(list, data.id);
+      // 更新修改后的数据
+      this.$emit("update:tree-data", list);
+    },
+    // 取消修改
+    cancel() {
+      this.currentId = "";
+    },
+    // 自定义渲染树显示内容
     render(h, { node, data, store }) {
-      const list = this.isParent(data) ? this.directoryDrop : this.fileDrop
+      const list = this.isParent(data) ? this.directoryDrop : this.fileDrop;
       return (
-        <div class='custom-tree-node'>
+        <div class="custom-tree-node">
           {this.isParent(data) ? (
             node.expanded ? (
-              <i class='el-icon-folder-opened'></i>
+              <i class="el-icon-folder-opened"></i>
             ) : (
-              <i class='el-icon-folder'></i>
+              <i class="el-icon-folder"></i>
             )
           ) : (
-            <i class='el-icon-document'></i>
+            <i class="el-icon-document"></i>
           )}
-          <span style='color: #f60'> {node.label}</span>
+          {/**修改树名称输入框 */}
+          {data.id === this.currentId ? (
+            <el-input
+              size="mini"
+              value={this.currentCont}
+              on-input={this.handleInput}
+            ></el-input>
+          ) : (
+            <span style="color: #f60"> {node.label}</span>
+          )}
           {
-            // <span>
-            //   <el-button
-            //     size="mini"
-            //     type="text"
-            //     on-click={() => this.append(data)}
-            //   >
-            //     Append
-            //   </el-button>
-            //   <el-button
-            //     size="mini"
-            //     type="text"
-            //     on-click={() => this.remove(node, data)}
-            //   >
-            //     Delete
-            //   </el-button>
-            // </span>
             // on-command={(value) => {
             //   this.handleCommand(data, value);
             // }}
+            // bind 绑定时候, 会把原来的参数向后移动
           }
-          {/* bind 绑定时候, 会把原来的参数向后移动 */}
-          <el-dropdown
-            trigger='click'
-            on-command={this.handleCommand.bind(this, data)}
-            placement='bottom-end'
-          >
-            <span class='el-dropdown-link'>
-              下拉菜单<i class='el-icon-arrow-down el-icon--right'></i>
+          {data.id !== this.currentId ? (
+            <el-dropdown
+              trigger="click"
+              on-command={this.handleCommand.bind(this, data)}
+              placement="bottom-end"
+            >
+              <span class="el-dropdown-link">
+                下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                {list.map((item) => (
+                  <el-dropdown-item command={item}>
+                    {item.value}
+                  </el-dropdown-item>
+                ))}
+              </el-dropdown-menu>
+            </el-dropdown>
+          ) : (
+            <span style={{ float: "right" }}>
+              <el-button size="mini" type="text" on-click={() => this.ok(data)}>
+                修改
+              </el-button>
+              <el-button size="mini" type="text" on-click={() => this.cancel()}>
+                取消
+              </el-button>
             </span>
-            <el-dropdown-menu slot='dropdown'>
-              {list.map((item) => (
-                <el-dropdown-item command={item}>{item.value}</el-dropdown-item>
-              ))}
-            </el-dropdown-menu>
-          </el-dropdown>
+          )}
         </div>
-      )
-    },
-    // 点击树获取数据
-    handleNodeClick(data) {
-      console.log('点击', data)
+      );
     },
     // 勾选树获取数据
     handleCheckChange(data) {
-      console.log('勾选', data)
-    }
-  }
-}
+      console.log("勾选树:", data);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-::v-deep .el-tree .custom-tree-node,
-::v-deep .el-tree .el-tree-node__content {
-  width: 360px;
-}
-::v-deep .el-dropdown {
-  float: right;
+.tree {
+  ::v-deep .el-tree .custom-tree-node,
+  ::v-deep .el-tree .el-tree-node__content {
+    width: 380px;
+    margin: 2px 0;
+  }
+  ::v-deep .el-dropdown {
+    float: right;
+  }
+  ::v-deep .el-input--mini {
+    width: 60%;
+    margin: 0 10px;
+    line-height: 16px;
+  }
 }
 </style>
--- admin/src/views/tree/custom.vue
+++ admin/src/views/tree/custom.vue
@@ -2,96 +2,106 @@
   <div class="tree">
     <div>数据组合树</div>
     <my-tree
-      :tree-data="treeData"
+      :tree-data.sync="treeData"
       :default-props="defaultProps"
       :file-drop="fileDrop"
       :directory-drop="directoryDrop"
-      :delete="deleteTreeRow"
+      :deleteTree="deleteTree"
+      :updateTree="updateTree"
     />
   </div>
 </template>
 
 <script>
-import MyTree from './component/MyTree'
+import MyTree from "./component/MyTree";
 export default {
   components: { MyTree },
   data() {
     return {
       defaultProps: {
-        children: 'children',
-        label: 'name'
+        children: "children",
+        label: "name",
       },
-      fileDrop: [{ text: 'rm', value: '删除文件' }],
+      fileDrop: [{ text: "rm", value: "删除文件" }],
       directoryDrop: [
-        { text: 'rn', value: '修改名字' },
-        { text: 'rm', value: '删除文件夹' }
+        { text: "rn", value: "修改名字" },
+        { text: "rm", value: "删除文件夹" },
       ],
       oData: {
         child: [
           // 文件
-          { name: '文件1', pid: 1, id: 10001 },
-          { name: '文件2', pid: 1, id: 10002 },
-          { name: '文件2-1', pid: 2, id: 10003 },
-          { name: '文件2-2', pid: 2, id: 10004 },
-          { name: '文件1-1-1', pid: 4, id: 10005 },
-          { name: '文件2-1-1', pid: 5, id: 10006 },
-          { name: '文件2-1-2', pid: 5, id: 100061 },
-          { name: '文件3-1-1', pid: 6, id: 10007 },
-          { name: '文件3-1-1-1', pid: 7, id: 100071 }
+          { name: "文件1", pid: 1, id: 10001 },
+          { name: "文件2", pid: 1, id: 10002 },
+          { name: "文件2-1", pid: 2, id: 10003 },
+          { name: "文件2-2", pid: 2, id: 10004 },
+          { name: "文件1-1-1", pid: 4, id: 10005 },
+          { name: "文件2-1-1", pid: 5, id: 10006 },
+          { name: "文件2-1-2", pid: 5, id: 100061 },
+          { name: "文件3-1-1", pid: 6, id: 10007 },
+          { name: "文件3-1-1-1", pid: 7, id: 100071 },
         ],
         parent: [
           // 文件夹
-          { name: '文件夹1', pid: 0, id: 1 },
-          { name: '文件夹2', pid: 0, id: 2 },
-          { name: '文件夹3', pid: 0, id: 3 },
-          { name: '文件夹1-1', pid: 1, id: 4 },
-          { name: '文件夹2-1', pid: 2, id: 5 },
-          { name: '文件夹3-1', pid: 3, id: 6 },
-          { name: '文件夹3-1-1', pid: 6, id: 7 }
-        ]
+          { name: "文件夹1", pid: 0, id: 1 },
+          { name: "文件夹2", pid: 0, id: 2 },
+          { name: "文件夹3", pid: 0, id: 3 },
+          { name: "文件夹1-1", pid: 1, id: 4 },
+          { name: "文件夹2-1", pid: 2, id: 5 },
+          { name: "文件夹3-1", pid: 3, id: 6 },
+          { name: "文件夹3-1-1", pid: 6, id: 7 },
+        ],
       },
-      treeData: []
-    }
+      treeData: [],
+    };
   },
   mounted() {
-    this.formatTreeData()
+    this.formatTreeData();
   },
   methods: {
     // 处理数据为树形数据
     formatTreeData() {
-      this.oData.parent.forEach((p) => (p.type = 'parent'))
-      const allData = [...this.oData.parent, ...this.oData.child]
+      this.oData.parent.forEach((p) => (p.type = "parent"));
+      const allData = [...this.oData.parent, ...this.oData.child];
       // 将两个数组合并为一个 键为id 值为整个对象
       const treeMapList = allData.reduce((prev, current) => {
-        prev[current.id] = current
-        return prev
-      }, {})
+        prev[current.id] = current;
+        return prev;
+      }, {});
       // 生成树结构
       const res = allData.reduce((arr, current) => {
-        const pid = current.pid
-        const parent = treeMapList[pid] // 获取父对象数据
+        const pid = current.pid;
+        const parent = treeMapList[pid]; // 获取父对象数据
         if (parent) {
           parent.children
             ? parent.children.push(current)
-            : (parent.children = [current])
+            : (parent.children = [current]);
         } else if (pid === 0) {
           // 顶级tree
-          arr.push(current)
+          arr.push(current);
         }
-        return arr
-      }, [])
-      this.treeData = res
+        return arr;
+      }, []);
+      this.treeData = res;
+    },
+    // 修改树名称
+    updateTree(id, newName) {
+      return new Promise((resolve, reject) => {
+        // 请求接口
+        setTimeout(() => {
+          resolve({ status: 0, msg: "修改成功" });
+        }, 1000);
+      });
     },
     // 删除树
-    deleteTreeRow(id) {
+    deleteTree(id) {
       return new Promise((resolve, reject) => {
         setTimeout(() => {
-          resolve({ status: 0, msg: '删除成功' })
-        }, 2000)
-      })
-    }
-  }
-}
+          resolve({ status: 0, msg: "删除成功" });
+        }, 1000);
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
